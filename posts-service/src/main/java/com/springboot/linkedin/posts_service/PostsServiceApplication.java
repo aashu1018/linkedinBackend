726ea874d68f7d0fcc5bbb1586dea6ffef3fd2d3
Ashutosh <ashu.ksing1018@gmail.com>
2025-07-10 00:04:27 +0530

    first commit

---
+++ posts-service/src/main/java/com/springboot/linkedin/posts_service/PostsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.linkedin.posts_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostsServiceApplication.class, args);
+	}
+
+}
