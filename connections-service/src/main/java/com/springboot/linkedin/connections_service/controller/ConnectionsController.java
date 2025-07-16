64c316ae523667f79e2a6f4ae3708662e734c296
Ashutosh <ashu.ksing1018@gmail.com>
2025-07-17 02:14:09 +0530

    connections service classes

--- connections-service/src/main/java/com/springboot/linkedin/connections_service/controller/ConnectionsController.java
+++ connections-service/src/main/java/com/springboot/linkedin/connections_service/controller/ConnectionsController.java
@@ -1,7 +1,15 @@
 package com.springboot.linkedin.connections_service.controller;
 
+import com.springboot.linkedin.connections_service.service.ConnectionsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/core")
+@RequiredArgsConstructor
 public class ConnectionsController {
+
+    private final ConnectionsService connectionsService;
+
 }
