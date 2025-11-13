70daccae29aba8b7ec961e9fdffa2b49ca429978
Ashutosh <ashu.ksing1018@gmail.com>
2025-11-14 00:42:39 +0530

    notification service

---
+++ notification-service/src/main/java/com/linkiedinApp/posts_service/event/PostLikedEvent.java
@@ -0,0 +1,14 @@
+package com.linkiedinApp.posts_service.event;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class PostLikedEvent {
+
+    Long postId;
+    Long creatorId;
+    Long likedByUserId;
+
+}
