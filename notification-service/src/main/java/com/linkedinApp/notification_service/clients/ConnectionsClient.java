27b4de2dea19c00ccb00c88b361249abdcf10cc1
Ashutosh <ashu.ksing1018@gmail.com>
2025-11-18 00:21:06 +0530

    notification service

---
+++ notification-service/src/main/java/com/linkedinApp/notification_service/clients/ConnectionsClient.java
@@ -0,0 +1,16 @@
+package com.linkedinApp.notification_service.clients;
+
+import com.linkedinApp.notification_service.dto.PersonDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "connections-service", path = "/connections")
+public interface ConnectionsClient {
+
+    @GetMapping("/core/{userId}/first-degree")
+    List<PersonDTO> getFirstConnections(@PathVariable Long userId);
+
+}
